be719e0b60748b2b149fa84285773ad5063b7313
LankeBhaskarVarma <bl6597@srmist.edu.in>
2026-03-01 22:01:46 +0530

    Add code for UC3

---
+++ UC3.java
@@ -0,0 +1,32 @@
+public class UC3 {    public static void main(String[] args) {
+        System.out.println(String.join("  ",
+                "  *** ",
+                "     *** ",
+                "   ****** ",
+                "  ***** "));
+
+        System.out.println(String.join("  ",
+                " **  ** ",
+                " **  ** ",
+                " **  ** ",
+                " **    "));
+
+        System.out.println(String.join("  ",
+                " **  ** ",
+                " **   ** ",
+                " ****** ",
+                "  ***** "));
+
+        System.out.println(String.join("  ",
+                " **  ** ",
+                " **  ** ",
+                "  **     ",
+                "     ** "));
+
+        System.out.println(String.join("  ",
+                "  ***  ",
+                "    ***   ",
+                " **     ",
+                "  ***** ")); //test
+    }
+}
